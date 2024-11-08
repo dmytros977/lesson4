9b316d9293fe8eccdb85401c80c06ba88da4f009
Dima <72711928+dmytros977@users.noreply.github.com>
2024-11-08 14:43:11 +0300

    Add files via upload

---
+++ module1_5.py
@@ -0,0 +1,11 @@
+example = 'Urban'
+print(example[0])
+print(example[-1])
+print(example[::-1])
+example = 'Топинамбур'
+print(example [1::2])
+print(example[0])
+print(example[-1])
+print(example[5::])
+print(example[::-1])
+print(example[1::2])
